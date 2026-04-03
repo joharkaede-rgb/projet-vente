035e7baeadc2c395def9a05d168b4fa15f06a1ad
joharkaede-rgb <joharkaede@gmail.com>
2026-04-03 18:57:44 +0300

    Create app.js

---
+++ public/app.js
@@ -0,0 +1,28 @@
+/Frontend JS - gestion produits, panier,
+estimation livraison et checkout mock /
+Const apiBase = ";
+let products = [1;
+let cart = [);
+|| Elements
+Const productsEl =
+document.getElementByld ('products');
+const cartCountEl =
+document.getElementByld ('cart-count');
+const viewCartBtn =
+document.getElementByld('view-cart-bt
+n);
+const cartModal =
+document.getElementByld('cart-modal');
+const closeCartBtn =
+document.getElementByld ('close-cart');
+const cartltemsEl =
+document.getElementByld ('cart-items');
+Const subtotalEl =
+document.getElementByld ('subtotal');
+const summaryDeliveryEI =
+document.getElementByld('summary-de
+livery');
+const summaryTotalEl =
+document.getElementByld ('summary-tot
+al);
+  
